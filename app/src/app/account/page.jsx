ba0685521764658d16ed3ed9d7ed5d9e2105d42b
Ana Ventura <anaventura1811@gmail.com>
2024-03-26 08:44:08 -0300

    feat: cria rotas básicas de login e criação de conta

--- app/src/app/account/page.jsx
+++ app/src/app/account/page.jsx
@@ -1,14 +1,16 @@
 "use client"; // This is a client component
 import React from 'react';
 import styles from './styles.module.css';
+import { useAuth } from '../context/Auth';
 
 const Account = () => {
-
+  const [{ user }] = useAuth();
   return (
     <main className={styles.main}>        
         <section>
               
-          <h1> Minha conta </h1>                                                                          
+          <h1> Minha conta </h1>                                                                   <h2>Email: {user.email}</h2>
+          <h2>{user.name}</h2>
         </section>
     </main>
   )
