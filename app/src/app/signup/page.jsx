ebec8e43cdf9cb4dd647e3c2958f6b11d09322c1
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-07 12:03:01 -0300

    feat: inicia estilização da tela de cadastro

--- app/src/app/signup/page.jsx
+++ app/src/app/signup/page.jsx
@@ -17,13 +17,27 @@ const SignUp = () => {
   };
 
   return (
-    <main className={styles.main}>        
-        <section className={styles.section}>
+    <main className="bg-custom-gray1 font-custom w-full h-screen">        
+        <section className="flex flex-col h-screen justify-around">
             <div>
                 <div>                  
-                    <h1> Matemania </h1>                                                                            
-                    <form className={styles.form}>                                                                                            
-                        <div className={styles.outerInput}>
+                    <form className="rounded flex flex-col items-center px-4 md:px-8 lg:px-12 xl:px-16 pt-4 pb-8 sm:mt-8 lg:mt-24">                                                                                            
+                          <h1> Matemania </h1>
+                          <div className="mb-8 w-full md:w-96 lg:w-120 xl:w-144">
+                            <label htmlFor="email" className={styles.inputLabel}>
+                                Nome
+                            </label>
+                            <input
+                                className={styles.input}
+                                type="email"
+                                label="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}  
+                                required                                    
+                                placeholder="Digite seu nome"                                
+                            />
+                        </div>  
+                        <div className="mb-8 w-full md:w-96 lg:w-120 xl:w-144">
                             <label htmlFor="email" className={styles.inputLabel}>
                                 Email
                             </label>
@@ -38,7 +52,7 @@ const SignUp = () => {
                             />
                         </div>
 
-                        <div className={styles.outerInput}>
+                        <div className="mb-8 w-full md:w-96 lg:w-120 xl:w-144">
                             <label htmlFor="password" className={styles.inputLabel}>
                                 Senha
                             </label>
@@ -54,22 +68,23 @@ const SignUp = () => {
                         </div>                                             
                         
                         <button
-                            className={styles.button}
+                            className="shadow-lg bg-custom-red hover:bg-custom-hoverBtn text-white py-3 px-2 w-full md:w-96 lg:w-96 xl:w-120 focus:outline-none mb-4 rounded-md text-sm"
                             type="submit" 
                             disabled={!password || !email}
                             onClick={createAccount}                        
-                        >  
-                        Cadastrar                               
+                            >  
+                            Cadastrar                               
                         </button>
+                        <p>
+                            Já possui uma conta?{' '}
+                            <Link href="/login" >
+                            Entre aqui
+                            </Link>
+                        </p>
                                                                     
                     </form>
                 
-                    <p>
-                        Já possui uma conta?{' '}
-                        <Link href="/login" >
-                        Entre aqui
-                        </Link>
-                    </p>                   
+                                      
                 </div>
             </div>
         </section>
