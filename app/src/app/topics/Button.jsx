cddfde72286c8d025ae278e12bc42ab616e85952
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2024-04-16 18:00:07 -0300

    feat: adiciona componente Button

---
+++ app/src/app/topics/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div className="mt-16 flex justify-center">
+        <button className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs">
+            Fazer treinamento
+        </button>
+    </div>
+  )
+}
+
+export default Button;
