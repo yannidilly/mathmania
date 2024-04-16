b5522db8ab479b42809efc505bfc969da7a2b709
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-16 19:40:00 -0300

    feat: adiciona link na logo Header

--- app/src/app/topics/Header.jsx
+++ app/src/app/topics/Header.jsx
@@ -2,24 +2,30 @@ import React from "react";
 import LogoImage from '../../assets/logo-mathmania-white.png';
 import Image from 'next/image';
 import ProfileImage from '../../assets/profilePic.jpeg';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className="bg-custom-red h-20 flex justify-between">
-      <div className="flex items-center">
-        <Image
-          className="p-6 w-40"
-          src={LogoImage}
-          alt="logo MathMania"
-        />
-      </div>
-      <div className="flex items-center">
-        <Image
-          className="p-6 w-20 rounded-full"
-          src={ProfileImage}
-          alt="Foto de perfil"        
-        />
-      </div>
+        <Link href="/login" className='flex items-center'>
+          <div>
+            <Image
+              className="p-6 w-40"
+              src={LogoImage}
+              alt="logo MathMania"
+            />
+          </div>
+      </Link>
+      
+      <Link href="/editInfo" className='flex items-center'>
+          <div className="flex items-center">
+            <Image
+              className="p-6 w-20 rounded-full"
+              src={ProfileImage}
+              alt="Foto de perfil"        
+            />
+          </div>
+      </Link>  
     </div>
   )
 }
