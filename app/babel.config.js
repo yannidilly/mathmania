24c2e3e9973b81229a08c17e99d0418bccd50f14
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-16 20:49:20 -0300

    chore: configura o babel.config

---
+++ app/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }], // Para JavaScript moderno
+    '@babel/preset-react' // Para habilitar o suporte ao JSX
+  ],
+};
