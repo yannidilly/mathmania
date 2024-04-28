a05782f5810fcc15bcb8ed55d899e9f3ec01686e
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-28 19:03:39 -0300

    refactor: refatora estrutura do componente CardSubject

--- app/src/app/topics/CardSubject.jsx
+++ app/src/app/topics/CardSubject.jsx
@@ -1,42 +1,26 @@
 import React from "react";
 
-const CardSubject = ({ title, subject }) => {
+const CardSubject = ({ subject }) => {
   return (
-    <main className="mt-9 ml-6 h-40">
-      <h2 className="text-custom-gray4">{title}</h2>
-      <span>
-        <hr className="border-custom-gray3" />
-      </span>
-      <div className="flex gap-6">
-      <div className="bg-custom-white p-4 space-y-2 w-40 rounded-lg mt-6 flex flex-col justify-center shadow-lg">
+    <div className="mt-4 md:mt-9 ml-4 md:ml-6 space-x-8"> {/* Adicionamos space-x-4 para criar espaço horizontal entre os cards */}
+      <div className="bg-custom-white p-4 md:p-6 space-y-2 md:w-72 lg:w-80 rounded-lg flex flex-col justify-center shadow-lg">
         <div className="flex justify-center">
-          <h2 className="text-custom-gray4 text-sm">{subject}</h2>
+          <h2 className="text-custom-gray4 text-sm md:text-base max-w-full">{subject}</h2>
         </div>
-        <div className="flex justify-evenly">
+        <div className="flex justify-evenly mt-4 mb-4">
           <span>⭐</span>
           <span>⭐</span>
           <span>⭐</span>
         </div>
-        <button className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs">
+        <button className="uppercase bg-custom-red text-white py-2 rounded text-xs md:text-sm mt-8">
           Iniciar
         </button>
       </div>
-      <div className="bg-custom-white p-4 space-y-2 w-40 rounded-lg mt-6 flex flex-col justify-center shadow-lg">
-        <div className="flex justify-center">
-          <h2 className="text-custom-gray4 text-sm">{subject}</h2>
-        </div>
-        <div className="flex justify-evenly">
-          <span>⭐</span>
-          <span>⭐</span>
-          <span>⭐</span>
-        </div>
-        <button className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs">
-          Iniciar
-        </button>
-      </div>
-      </div>
-    </main>
+    </div>
   );
 };
 
 export default CardSubject;
+
+
+
