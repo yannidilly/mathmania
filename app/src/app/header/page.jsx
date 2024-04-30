832d39e9e79892de938d5e0770efabcce5ed1bdb
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-30 19:50:37 -0300

    chore: remove comentários

--- app/src/app/header/page.jsx
+++ app/src/app/header/page.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { useAuth } from '../context/Auth';
 
 const Header = () => {
-  const [{ user }] = useAuth(); // Obtenha o usuário do contexto de autenticação
+  const [{ user }] = useAuth();
 
   return (
     <div className="bg-custom-red h-20 flex justify-between">
@@ -21,10 +21,10 @@ const Header = () => {
         </div>
       </Link>
       
-      {user && ( // Verifica se há um usuário logado
+      {user && (
         <Link href="/editInfo" className='flex items-center'>
           <div className="flex items-center">
-            {/* Use a foto do usuário se estiver disponível, caso contrário, use uma imagem padrão */}
+            
             <Image
               className="p-6 w-20 rounded-full"
               src={user.avatar || ProfileImage}
