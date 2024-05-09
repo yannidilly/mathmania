2805945144f8fd7493b4ebf8353d0ca1630c87d1
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2024-05-09 18:38:06 -0300

    Fix: substitui header antigo pelo novo

--- app/src/app/subjects/page.jsx
+++ app/src/app/subjects/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Header from "./Header";
+import Header from "../header/page";
 import CardSubject from "./CardSubject";
 import Button from "./Button";
 import Slider from "react-slick";
