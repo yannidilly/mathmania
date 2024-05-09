f3b790f18fef4b3971643b7af702ad8fde92963a
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2024-05-09 18:16:20 -0300

    Fix: corrige nome da rota para subjects

--- app/src/app/subjects/page.jsx
+++ app/src/app/subjects/page.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
-import { collection, addDoc } from "firebase/firestore"; 
+"use client";
+import React from "react";
+import Header from "./Header";
+import CardSubject from "./CardSubject";
+import Button from "./Button";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import TitleSubject from './titleSubject';
+import { collection, addDoc } from "firebase/firestore";
 
-const subjects = () => {
+const Topics = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
 
   return (
-    <div>
-      <h1>Subjects</h1>
-    </div>
-  );
-}
+    <main className="bg-custom-gray1 w-full min-h-screen font-custom">
+      <Header />
+      
+      <div className="container mx-auto px-4 py-8">
+        <TitleSubject title="Novas Matérias" />
 
-export default subjects;
+        <Slider {...settings}>
+          <CardSubject subject="Geometria" />
+          <CardSubject subject="Trigonometria" />
+          <CardSubject subject="Estatística" />
+          <CardSubject subject="Probabilidade" />
+        </Slider>
+      </div>
+
+      <div className="container mx-auto px-4 py-8">
+        <TitleSubject title="Continuar Estudos" />
+
+        <Slider {...settings}>
+          <CardSubject subject="Raiz Quadrada" />
+          <CardSubject subject="Conjuntos" />
+          <CardSubject subject="Equações" />
+          <CardSubject subject="Aritimética" />
+        </Slider>
+      </div>
+
+      <div className="container mx-auto px-4 py-8">
+        <TitleSubject title="Revisão" />
+
+        <Slider {...settings}>
+          <CardSubject subject="Análise Real" />
+          <CardSubject subject="Álgebra Linear" />
+          <CardSubject subject="Cálculo" />
+          <CardSubject subject="Cálculo Integral" />
+        </Slider>
+      </div>
+          <Button />  
+    </main>
+  );
+};
+
+export default Topics;
