ba71294fe5bdfeae738cc51b1479c98aacfe361a
lbseven7 <leobarbosapinturas@gmail.com>
2024-05-09 21:10:36 -0300

    refactor: button

--- app/src/app/button/page.jsx
+++ app/src/app/button/page.jsx
@@ -4,7 +4,6 @@ const Button = ({title}) => {
   return (
     <div className="mt-16 flex justify-center">
       <button
-        type="submit"
         className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs mb-24">
           {title}
         </button>
