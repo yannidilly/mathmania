10ea03aa11dc6cb2ea9adc6c3cb754b03ad3be99
lbseven7 <leobarbosapinturas@gmail.com>
2024-05-30 15:29:05 -0300

    feat: Adiciona link para editar informações na página de conta

--- app/src/app/account/page.jsx
+++ app/src/app/account/page.jsx
@@ -1,18 +1,24 @@
-
+'use client';
 import React from 'react';
 import { useAuth } from '../context/Auth';
 
 const Account = () => {
-  const { user , updateAccountInfo } = useAuth();
-  // console.log('email: ', user);
+  const { user } = useAuth();
+  
   return (
     <main className='text-lg'>        
-        <section>
-          <h1> Minha conta </h1>                                                                   
-          <h2 onClick={updateAccountInfo}>Email: {user?.email}</h2>
-          <h2>{user?.name}</h2>
-        </section>
+      <section>
+        <h1> Minha conta </h1>                                                                   
+        <h2>Email: {user?.email}</h2>
+        <h2>{user?.name}</h2>
+        
+        <a
+          href="/account/edit"
+          className="text-blue-500 hover:underline mt-4">Editar Informações</a>
+        
+      </section>
     </main>
-  )
+  );
 };
+
 export default Account;
