bbf9a83f5ce8cab78a999ea36f3d5222964dbbb8
lbseven7 <leobarbosapinturas@gmail.com>
2024-05-30 17:50:39 -0300

    refactor: atualiza estilos e margens do componente Button

--- app/src/app/button/page.jsx
+++ app/src/app/button/page.jsx
@@ -4,7 +4,7 @@ const Button = ({title}) => {
   return (
     <div className="mt-16 flex justify-center">
       <button
-        className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs mb-24">
+        className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs font-bold mb-24 w-60">
           {title}
         </button>
     </div>
