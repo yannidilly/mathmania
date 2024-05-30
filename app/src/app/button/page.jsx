48c82eaa89e69756f17b0eea37de99576278df60
lbseven7 <leobarbosapinturas@gmail.com>
2024-05-30 18:27:10 -0300

    refactor: atualiza estilos e margens do componente Button

--- app/src/app/button/page.jsx
+++ app/src/app/button/page.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Button = ({title}) => {
   return (
-    <div className="mt-16 flex justify-center">
+    <div className="mt-8 flex flex-col justify-center">
       <button
-        className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs font-bold mb-24 w-60">
+        className="uppercase bg-custom-red text-white py-2 px-4 rounded text-xs font-bold w-60">
           {title}
         </button>
     </div>
